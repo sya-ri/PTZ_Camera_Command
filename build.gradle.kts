d7aaa7fc6e9df9b96b5a84870e1ae018ec1cb18a
sya-ri <sya79lua@gmail.com>
2020-10-13 20:55:09 +0900

    shadowJar を作成できるようにした

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "me.syari.ptz_camera_command"
@@ -17,4 +18,14 @@ dependencies {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to "${project.group}.MainKt"
+            )
+        )
+    }
 }
